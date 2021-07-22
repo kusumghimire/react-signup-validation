a3dc37f79f2e142c919df81f6dbed0fb32cab782
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-07-22 23:30:47 +0545

    create: signupformsuccess is imported into App.js

--- src/components/form.js
+++ src/components/form.js
@@ -1,10 +1,11 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import SignupFormSuccess from './SignupFormSuccess';
 import SignUpForm from './SignupForm'
 const Form = ()=>{
+    const [formIsSubmitted, setFormIsSubmitted] = useState(false);
     return(
         <>
-        <SignUpForm />
+        {!formIsSubmitted ?  <SignupFormSuccess /> : <SignupForm/> }   
         </>
     )
 }
