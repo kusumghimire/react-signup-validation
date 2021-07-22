20f278dc61b03b83b29e6a7aea8f937f0a2a0fb2
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-07-22 22:25:24 +0545

    input field is created and updated

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -6,9 +6,32 @@ const SignupForm =() =>{
           <div className="app-wrapper">
              <div>
                <h2 className='title'>
-Create Account
+                 Create Account
                </h2>
              </div>
+
+             <form className='form-wrapper'>
+               <div className='name'>
+                 <label className='label'>
+                   Full Name
+                 </label>
+                 <input className='input' type='text' />
+               </div>
+               <div className='name'>
+                 <label className='label'>
+                  Email
+                 </label>
+                 <input className='input' type='email' />
+               </div>
+               <div className='name'>
+                 <label className='label'>
+                 Password
+                 </label>
+                 <input className='input' type='password' />
+               </div>
+
+             </form>
+
           </div>
 
 
